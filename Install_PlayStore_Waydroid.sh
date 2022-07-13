a1221ea66d20ff77180517cf2307c066cb139d9d
Vitor Veloso <vitorveloso@disroot.org>
2022-07-13 14:14:38 -0300

    fix playstore to install full version

--- Install_PlayStore_Waydroid.sh
+++ Install_PlayStore_Waydroid.sh
@@ -1,12 +1,13 @@
 #sudo pacman -S qemu
+###DEBUG
 rm -rf ./opengapps/
-sudo rm -rf /tmp/opengapps
+#sudo rm -rf /tmp/opengapps
+###
 
-
-#ZERO_BINS >>>> find /proc/sys/fs/binfmt_misc -type f -name 'arm*' -exec sudo sh -c 'echo -1 > {}' \;
 
 waydroid session stop
 sudo systemctl stop waydroid-container.service
+sudo rm -rf /tmp/opengapps/
 
 IMG=$(cat /var/lib/waydroid/waydroid.cfg | grep images_path | cut -d' ' -f 3)/system.img
 IMG_VENDOR=$(cat /var/lib/waydroid/waydroid.cfg | grep images_path | cut -d' ' -f 3)/vendor.img
@@ -15,7 +16,7 @@ TMP_DIR=/tmp/opengapps/system/
 
 
 ARCH=$(cat /var/lib/waydroid/lxc/waydroid/config | grep lxc.arch| cut -d " " -f 3)
-VARIANT="pico"
+VARIANT="full"
 ANDROID_VERSION="10.0"
 
 while [ "$(mount | grep waydroid | cut -d" " -f3)" != "" ];do
@@ -43,11 +44,11 @@ fi
 
 echo "extracting open gapps"
 
-
+#DEBUG
 sudo rm -rf opengapps
-
 unzip -d opengapps ./$OPENGAPPS_FILE
 
+#DEBUG
 
 for filename in ./opengapps/Core/*.tar.lz
 do
@@ -66,14 +67,14 @@ do
 done
 
 
-
+#END DEBUG
 
 
 
 
 ##########Select and copy to tmp
 
-DIRS="etc framework product lib64 app priv-app"
+DIRS="etc framework product lib64 lib app priv-app"
 mkdir /tmp/opengapps
 mkdir $TMP_DIR
 
@@ -81,9 +82,13 @@ mkdir $TMP_DIR
  for i in $DIRS; do
 
 	mkdir $TMP_DIR/$i
- 	find opengapps -name $i -type d -exec  cp -aR $PWD/{}/ $TMP_DIR/ \; 
+	#######DEBUG
+	#find opengapps -name $i -type d -exec echo cp -aR $PWD/{}/ $TMP_DIR/ \; 
+	find opengapps -name $i -type d -exec  cp -aR $PWD/{}/ $TMP_DIR/ \; 
 	
  done
+
+	find opengapps -name overlay -type d -exec echo cp -aR $PWD/{}/ $TMP_DIR/product/overlay/ \; 
 
 
 #sudo chown 0:0 -R $TMP_DIR
@@ -106,13 +111,28 @@ mkdir /tmp/opengapps/system/etc/init/
 
 ########################
 
-SIZE=$(echo $(du -h -d0 /tmp/opengapps/system/ | cut -d'M' -f1)) 
-SIZE=$(( $SIZE + 10 ))
-SIZE=$SIZE\M
+SIZE=$(echo $(du -h -d0 /tmp/opengapps/system/  | cut -f1 | head -c -1))
+G_OR_M=( ${SIZE: -1})
+
+echo $SIZE
+if [[ "$G_OR_M"  == "G" ]] ; then
+	S_ADD=1
+SIZE=$(echo $SIZE | cut -d"," -f1)
+else
+	S_ADD=100
+fi
+SIZE=$(echo $SIZE + $S_ADD |bc)
+SIZE=$SIZE"$G_OR_M"
+SIZE=$(echo $SIZE | sed 's/\./\,/g')
+SIZE=$(echo $SIZE | sed 's/\,G/\G/g')
 echo adding \+ $SIZE to system.img 
+#debug
 sudo qemu-img resize $IMG +$SIZE
 sudo e2fsck -f $IMG
 sudo resize2fs $IMG
+
+# FROM Wachid (waydroid telegram) to add when i got time
+# resize2fs /var/lib/waydroid/images/system.img $(($original_size+$needed_spaces))M
 
 sudo mount $IMG $MOUNT_DIR
 
@@ -130,13 +150,17 @@ for i in $APPS; do sudo rm -rf /tmp/waydroid_system/system/priv-app/$i*;done
 ######################
 
 
+
 ###########
 #REMOVE OLD, view perms later
 ####################
 
-sudo rm -rf  $MOUNT_DIR/system/priv-app/GooglePackageInstaller
+#sudo rm -rf  $MOUNT_DIR/system/priv-app/GooglePackageInstaller
 sudo rm -rf  $MOUNT_DIR/system/priv-app/PackageInstaller
 
+
+
+sudo chown 0:0 -R /tmp/opengapps/
 
 ##################
 #MOVE TO IMG
@@ -146,6 +170,8 @@ sudo cp -aR $TMP_DIR $MOUNT_DIR
 #SPOOF
 #echo ro.build.fingerprint="OnePlus/OnePlus7Pro_EEA/OnePlus7Pro:10/QKQ1.190716.003/1910071200:user/release-keys" | sudo tee -a /var/lib/waydroid/waydroid_base.prop
 
+#wont start
+sudo rm -rf  $MOUNT_DIR/system/priv-app/WellbeingPrebuilt
 
 
 ############
